4eed8248091538fb8489e0ed26fafc644afd3785
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2022-09-19 20:33:02 +0100

    added auth0 for the front end

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-zk1xgmkt.us', // the auth0 domain prefix
+    audience: 'http://localhost:5000', // the audience set for the auth0 app
+    clientId: 'tV6z2XpJPWc9bKXzQch3RyVmB5XS5W53', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
